978d006d955c7de6632bf5d173b133fb8fcca572
Harshit Sharma <sharmaharshitnone@gmail.com>
2024-10-03 00:57:02 +0530

    Update main.cpp
    this is 2nd addition from my phone

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    cout<<"I love you AKANK";
+    cout << "I love you AKANK" << endl;
+
+    // Add two numbers
+    int num1, num2, sum;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    cout << "The sum is: " << sum << endl;
+
     return 0;
 }
